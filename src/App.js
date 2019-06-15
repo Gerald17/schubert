17f8c3762b5d5923989e491fb34e1892d364fe79
GerardoEstrada <gestrada@applaudostudios.com>
2019-06-15 12:58:00 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import WorkDay from './components/workday/WorkDay';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Ant
+import { Layout, Breadcrumb } from 'antd';
+
+//Components
+import SideMenu from "./components/menu/menu";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+class App extends Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = collapsed => {
+    this.setState({ collapsed });
+  };
+
+  render() {
+    const date = new Date();
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Router>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}
+          >
+              {<SideMenu/>}
+          </Sider>
+          <Layout>
+            <Header style={{ padding: 0 }} />
+            <Content style={{ margin: "0 16px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>Users</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>                
+                <Route exact path="/WorkDay" component={WorkDay} />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+            {`Schubert ©${date.getFullYear()}`}
+            </Footer>
+          </Layout>
+        </Router>
+      </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Menu, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+
+const SubMenu = Menu.SubMenu;
+
+const menuItems = [
+  {
+    id: 1,
+    label: "Iniciar Jornada",
+    icon: "pie-chart",
+    component: null,
+    showInMenu: true,
+    link: "WorkDay"
+  },
+  {
+    id: 2,
+    label: "Terminar Jornada",
+    icon: "pie-chart",
+    component: null,
+    showInMenu: true,
+    link: "WorkDay"
+  },
+  {
+    id: 3,
+    label: "Catalogos",
+    icon: "pie-chart",
+    component: null,
+    showInMenu: true,
+    link: null,
+    submenu: [
+      { id: 31, label: "Personas", icon: "pie-chart", component: null, showInMenu: true, link: "Persons" },
+      { id: 32, label: "Camiones", icon: "pie-chart", component: null, showInMenu: true, link: "Trucks" }
+    ]
+  }
+];
+
+const SideMenu = () => {
+  return (
+    <>
+      <div className="logo" style={{ color: "white" }}>
+        SCHUBERT
+      </div>
+      
+      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+        {menuItems.map(menuItem => {         
+          return (
+          menuItem.hasOwnProperty("submenu") ? 
+              <SubMenu
+                key={menuItem.id}
+                title={
+                  <span>
+                    <Icon type={menuItem.icon} />
+                    <span>{menuItem.label}</span>
+                  </span>
+                }
+              >
+                {menuItem.submenu.map(submenu => {
+                  return (
+                      <Menu.Item key={submenu.id}>
+                        <Link to={submenu.link}>{submenu.label}</Link>
+                      </Menu.Item>
+                    )
+                })}
+              </SubMenu>
+            :
+              <Menu.Item key={menuItem.id}>
+                <Link to={menuItem.link}>
+                  <Icon type={menuItem.icon} />
+                  <span>{menuItem.label}</span>
+                </Link> 
+              </Menu.Item>
+            )
+        })}
+      </Menu>
+    </>
+  );
+};
+
+export default SideMenu;
